f1bd0e86f807c661259b3c5b2ae553344b69df2f
VelkyCajicek <peichl294@gmail.com>
2024-09-18 21:49:17 +0200

    Working grid maps

--- Library/main.py
+++ Library/main.py
@@ -2,28 +2,26 @@ import pygame
 import map_generation as mg
 import roads as rd
 
+pygame.init()
+
 class GUI():
     def __init__(self, screen_size : int, grid_array_size : int) -> None: # Assuming the screen is square
         self.screen_size = screen_size
-        self.grid_array_size = grid_array_size
         self.screen = pygame.display.set_mode([screen_size, screen_size])
-        self.grid_map = mg.Map_Generation(self.grid_array_size).grid
+        self.grid_map = mg.Map_Generation(grid_array_size).grid
         self.mainLoop()
     
-    def generate_map(self):
-        block_size = self.screen_size / self.grid_array_size
-        print(block_size)
-        for j in range(0, self.grid_array_size):
-            i = 0
-            rd.Dashed(self.screen, [block_size / 2, block_size / 2], (j*block_size, i*block_size, (j+1)*block_size, (i+1)*block_size)) 
+    def generate_map(self) -> None:
+        block_size = self.screen_size / len(self.grid_map)
+        for i in range(0, len(self.grid_map)):
+            for j in range(0, len(self.grid_map[i])):
+                if(self.grid_map[i][j] == 1):
+                    rd.Dashed(self.screen, block_size, (j*block_size, i*block_size, block_size, block_size))  # First two start position and then size
 
-    
     def mainLoop(self) -> None:        
-        self.screen.fill((255, 255, 255)) # Color of the background
+        self.screen.fill((255, 178, 102)) # Color of the background
         self.generate_map()
         pygame.display.flip()
-       
-        self.DisplayList(self.grid_map)
         
         running = True
         while running: # main game loop
@@ -33,10 +31,5 @@ class GUI():
         
         pygame.quit()
         
-    def DisplayList(self, array):
-        for row in array:
-            print(row)
-        
-
 if __name__ == "__main__":
     gui = GUI(500, 10)
--- Library/roads.py
+++ Library/roads.py
@@ -1,16 +1,21 @@
 import pygame
 
 class Road():
-    def __init__(self, screen : pygame.Surface, section_size : list, position : tuple) -> None:
+    def __init__(self, screen : pygame.Surface, block_size : int, position : tuple) -> None:
         self.screen = screen
-        self.section_size = section_size
+        self.block_size = block_size
         self.position = position
         pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(self.position))
+    
+    #def neighbors(radius, row_number, column_number):
+    #    return [[a[i][j] if  i >= 0 and i < len(a) and j >= 0 and j < len(a[0]) else 0
+    #        for j in range(column_number-1-radius, column_number+radius)]
+    #            for i in range(row_number-1-radius, row_number+radius)]
      
 class Dashed(Road):
-    def __init__(self, screen, section_size, position) -> None:
-        super().__init__(screen, section_size, position)
-        self.dash_position = (self.position[0] / 2 - self.position[0] * 0.1, self.position[1] / 2 + self.position[1] * 0.1, self.position[2] / 2 + self.position[2] * 0.1, self.position[3] / 2 - self.position[3] * 0.1)
+    def __init__(self, screen, block_size, position) -> None:
+        super().__init__(screen, block_size, position)
+        self.dash_position = (self.position[0] + block_size / 5 * 2, self.position[1] + block_size / 5 + 10, 16, 4)
         pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(self.dash_position))
         
             
